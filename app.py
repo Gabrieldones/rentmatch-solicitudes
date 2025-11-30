d27e185cf3c187b9884f10f61e5a51596724e811
Gabrieldones <gabrieldones@hotmail.com>
2025-11-30 17:03:17 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Asistente Inmobiliario Inteligente 🏡🤖")
+st.write("Módulo de solicitudes de inquilinos: 'Quiero este piso'.")
+
+st.info("La app está instalada correctamente. A continuación añadiremos el formulario.")
